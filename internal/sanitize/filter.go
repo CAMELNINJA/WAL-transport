6f71241d712918a736efd5e52716989fe599c11e
Camel Ninla <rfvbkm0220@gmail.com>
2023-04-16 23:53:47 +0300

    feat: add filters

--- internal/sanitize/filter.go
+++ internal/sanitize/filter.go
@@ -5,13 +5,31 @@ import "github.com/CAMELNINGA/cdc-postgres.git/internal/models"
 // FilterHandler is a handler that filters out certain data from the request
 type FilterHandler struct {
 	BaseHandler
-	filter string
+	Table         string
+	filterColumns FilterColumns
+}
+
+type FilterColumns []*FilterColumn
+type FilterColumn struct {
+	Column string
 }
 
 // Handle checks if the request contains the filter and removes it if it does
+// Todo filter tables and columns
 func (h *FilterHandler) Handle(request models.ActionData) models.ActionData {
-	// if h.filter != "" {
-	// 	request = strings.Replace(request, h.filter, "", -1)
-	// }
+
+	if h.Table != "" || h.Table == request.Table {
+		if h.filterColumns != nil {
+			for _, v := range h.filterColumns {
+				for i, v2 := range request.NewColumns {
+					if v.Column == v2.Name {
+						request.NewColumns = append(request.NewColumns[:i], request.NewColumns[i+1:]...)
+					}
+				}
+			}
+		} else {
+			request.NewColumns = nil
+		}
+	}
 	return h.BaseHandler.Handle(request)
 }
--- internal/sanitize/replace.go
+++ internal/sanitize/replace.go
@@ -7,14 +7,31 @@ import (
 // ReplaceHandler is a handler that replaces certain values in the request with new ones
 type ReplaceHandler struct {
 	BaseHandler
-	old string
-	new string
+	oldTable       string
+	newTable       string
+	replaseColumns ReplaseColumns
 }
+
+type ReplaseColumn struct {
+	OldColumn string
+	NewColumn string
+}
+
+type ReplaseColumns []*ReplaseColumn
 
 // Handle checks if the request contains the old value and replaces it with the new one if it does
 func (h *ReplaceHandler) Handle(request models.ActionData) models.ActionData {
-	if h.old != "" {
-		// request = strings.Replace(request, h.old, h.new, -1)
+	if h.oldTable != "" || h.oldTable == request.Table || h.oldTable == "*" {
+		request.Table = h.newTable
+		if h.replaseColumns != nil {
+			for _, v := range h.replaseColumns {
+				for _, v2 := range request.NewColumns {
+					if v.OldColumn == v2.Name {
+						v2.Name = v.NewColumn
+					}
+				}
+			}
+		}
 	}
 	return h.BaseHandler.Handle(request)
 }
