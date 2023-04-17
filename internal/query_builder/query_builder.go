2f48038124ea3a29cfc3bcf6a2548eab437da58c
Camel Ninla <rfvbkm0220@gmail.com>
2023-04-18 00:54:32 +0300

    feat: prepare qurey build

--- internal/query_builder/query_builder.go
+++ internal/query_builder/query_builder.go
@@ -22,6 +22,10 @@ func (qb *qb) QueryBuilder(tx *models.ActionData) (string, []interface{}, error)
 	switch tx.Kind {
 	case models.ActionKindInsert:
 		return prepareQueryInsert(tx)
+	case models.ActionKindUpdate:
+		return prepareQueryUpdate(tx)
+	case models.ActionKindDelete:
+		return prepareQueryDelete(tx)
 	default:
 		return "", []interface{}{}, fmt.Errorf("not build")
 	}
@@ -35,4 +39,18 @@ func prepareQueryInsert(tx *models.ActionData) (string, []interface{}, error) {
 		values = append(values, v.Value)
 	}
 	return sq.Insert(tx.Table).Columns(columns...).Values(values...).ToSql()
+}
+
+func prepareQueryUpdate(tx *models.ActionData) (string, []interface{}, error) {
+	var values []interface{}
+	var columns []string
+	for _, v := range tx.NewColumns {
+		columns = append(columns, v.Name)
+		values = append(values, v.Value)
+	}
+	return sq.Update(tx.Table).SetMap(sq.Eq{"id": 1}).ToSql()
+}
+
+func prepareQueryDelete(tx *models.ActionData) (string, []interface{}, error) {
+	return sq.Delete(tx.Table).Where(sq.Eq{"id": 1}).ToSql()
 }
